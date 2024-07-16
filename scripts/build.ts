7d01411e3f223159fa1196f58c6f714f2c190e67
rainbowatcher <rainbow-w@qq.com>
2024-07-16 11:07:00 +0800

    build: make build target compability with nodejs

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,10 +1,41 @@
+import { readFile, writeFile } from "node:fs/promises"
+import process from "node:process"
 import { $ } from "execa"
-import { ensureDir } from "fs-extra"
+import { ensureDir, ensureFile } from "fs-extra"
 import { rimraf } from "rimraf"
+import { version } from "../package.json"
 
-const targetDir = "./tmp/qijs"
-await Promise.all([
-    ensureDir(targetDir),
-    rimraf(targetDir),
-    $`wasm-pack build crates/core --no-typescript --no-pack --dev -d ../../tmp/qijs -t web`,
-])
+const targetDir = "packages/qijs"
+const indexFilePath = `${process.cwd()}/${targetDir}/qi.js`
+const pkgJsonPath = `${process.cwd()}/${targetDir}/package.json`
+const pkgJsonContent = JSON.stringify({
+    files: [
+        "qi_bg.wasm",
+        "qi.js",
+        "qi.d.ts",
+    ],
+    main: "qi.js",
+    name: "qi",
+    type: "module",
+    types: "qi.d.ts",
+    version,
+}, null, 2)
+
+await ensureDir(`${process.cwd()}/${targetDir}`)
+await rimraf(`${process.cwd()}/${targetDir}`)
+await $`wasm-pack build crates/core --no-pack --dev -d ../../${targetDir} -t web`
+const indexStr = await readFile(indexFilePath, "utf8")
+const fetchExpr = "input = fetch(input);"
+if (indexStr.includes(fetchExpr)) {
+    const newIndexStr = indexStr.replace(fetchExpr, `if (globalThis.process?.release?.name === "node") {
+            const fs = (await import('fs')).default;
+            input = fs.readFileSync(input);
+        } else {
+            input = fetch(input);
+        }`)
+    await writeFile(indexFilePath, newIndexStr)
+} else {
+    throw new Error("input is not defined in the generated js file")
+}
+await ensureFile(pkgJsonPath)
+await writeFile(pkgJsonPath, pkgJsonContent)
